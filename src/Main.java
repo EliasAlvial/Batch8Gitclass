7c79342ef29256ddc3c67276fac21cd4fa9bb8de
EliasAlvial <Elias.Alvial@gmail.com>
2021-01-27 16:27:40 -0500

    user2 changes

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Batch 8");
         System.out.println("I am making some changes in my code");
+	System.out.println("making changes through textEdit");
     }
 }
